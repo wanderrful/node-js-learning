3d353789462f00286376f69be1970b0f07281200
Wanderrful <michael.lee0@gmail.com>
2016-05-04 15:39:09 +0900

    Initial

---
+++ meadowlark.js
@@ -0,0 +1,58 @@
+const express = require('express'),
+      app = express(),
+      handlebars = require('express-handlebars').create( { defaultLayout: 'main' } ),
+      fortunes = require('./resources/fortunes')
+
+
+
+
+app
+//set the server's initial parameters
+     .engine('handlebars', handlebars.engine)
+     .set('view engine', 'handlebars')
+     .set('port', process.env.PORT || 3000)
+     .use(express.static(__dirname + '/public'))
+//route individual server GET requests
+     .get(
+	  '/',
+	  (req, res) =>
+	       {
+		    res.render('home');
+	       }
+     )
+     .get(
+	  '/about',
+	  (req, res) =>
+	       {
+		    res.render(
+			 'about',
+			 {
+			      fortune: fortunes.getRandomFortune()
+			 }
+		    );
+	       }
+     )
+//handle HTTP request errors
+     .use(  //custom 404 page
+	  (req, res) =>
+	       {
+		    res.status(404);
+		    res.render('404');
+	       }
+     )
+     .use(  //custom 500 page
+	  (err, req, res, next) =>
+	       {
+		    console.error(err.stack)
+		    res.status(500);
+		    res.render('500');
+	       }
+     )
+//start the web server
+     .listen(
+	  app.get('port'),
+	  () =>
+	       {
+		    console.log('***Express server started on http://localhost' + app.get('port') + '\n(press C-c C-c to terminate)')
+	       }
+     )
